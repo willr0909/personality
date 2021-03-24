b091f0d94229d8609e082abe68366923f58bb0f6
p.m.student <wramirez@students.tulsatech.edu>
2021-03-24 14:35:39 -0500

    layout finished

---
+++ personality/IntroViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personality
+//
+//  Created by P.M. Student on 3/24/21.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
